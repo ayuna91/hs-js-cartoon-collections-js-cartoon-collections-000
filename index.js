c3d97de02e1d5a68ec5369d77779a8389ab651ef
Ayuna Tsyrenova <ayuna91@gmail.com>
2020-08-01 19:24:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,63 @@
-function dwarfRollCall(dwarves) {
+function dwarfRollCall(dwarves){
+var newArray = [];
+for(let i=0; i<dwarves.length; i++){
+  newArray.push(`${i+1}. ${dwarves[i]} `)
 }
+return newArray.join('');
+}
+ 
+
+
 
 function summonCaptainPlanet(planeteerCalls){
+  
+ for(let i=0; i<planeteerCalls.length; i++){
+   planeteerCalls[i] = `${planeteerCalls[i].toUpperCase()}!`;
+ }
+ return planeteerCalls;
 }
 
-function longPlaneteerCalls(words) {
+
+
+
+
+function longPlaneteerCalls(words){
+  var result = false;
+  for(let i=0; i<words.length; i++){
+    if(words.length>4){
+      result = true;
+      return result;
+    }else{
+      return false;
+    }
+  }
 }
 
-function findTheCheese (foods) {
+
+function findTheCheese(food){
+ var result = null;
+    for(let i=0; i<food.length; i++){
+        if( (food[i]==='gouda') || (food[i]==='cheddar') || (food[i]==='camembert') ){
+          result = food[i];
+          
+        }
+    }  
+  if(result === null){
+  return 'no cheese!';
+    }else{
+      return result;
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
