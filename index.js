3e98e6aaf2d4fabce15ca63eea44549984dc775b
Ayuna Tsyrenova <ayuna91@gmail.com>
2020-08-03 13:18:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 function dwarfRollCall(dwarves){
+var counter = 1;
 var newArray = [];
-for(let i=0; i<dwarves.length; i++){
-  newArray.push(`${i+1}. ${dwarves[i]} `)
+for(let i=0; i<=dwarves.length-1; i++){
+  newArray.push(`${counter++}. ${dwarves[i]} `)
+  
 }
 return newArray.join('');
 }
  
 
 
-
 function summonCaptainPlanet(planeteerCalls){
-  
- for(let i=0; i<planeteerCalls.length; i++){
-   planeteerCalls[i] = `${planeteerCalls[i].toUpperCase()}!`;
- }
- return planeteerCalls;
+  for(let i=0; i<planeteerCalls.length; i++){
+    planeteerCalls[i]=`${planeteerCalls[i].toUpperCase()}!`
+  }
+return planeteerCalls;
 }
 
 
@@ -22,42 +22,32 @@ function summonCaptainPlanet(planeteerCalls){
 
 
 function longPlaneteerCalls(words){
-  var result = false;
-  for(let i=0; i<words.length; i++){
-    if(words.length>4){
-      result = true;
-      return result;
-    }else{
-      return false;
-    }
-  }
+ var result = false;
+ 
+ for(let i=0; i<words.length; i++){
+   if(words[i].length>4){
+     result = true;
+   }
+ }
+   return result;
 }
+
+
+
 
 
 function findTheCheese(food){
- var result = null;
-    for(let i=0; i<food.length; i++){
-        if( (food[i]==='gouda') || (food[i]==='cheddar') || (food[i]==='camembert') ){
-          result = food[i];
-          
-        }
-    }  
-  if(result === null){
-  return 'no cheese!';
-    }else{
-      return result;
-    }
+
+var result = null;
+ for(let i=0; i<food.length; i++){
+   if( (food[i]==='cheddar') || (food[i]==='gouda') || (food[i]==='camembert')  ){
+     result = food[i];
+     break;
+   }
+ }
+ if(result === null){
+   return 'no cheese!'
+ }else{
+   return result;
+ }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
